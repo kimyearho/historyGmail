a5259bee60d4f8cd1a5d905e41ce2dd93b33d01d
yeonho <yeonho@in-soft.com>
2017-10-19 18:23:44 +0900

    구글 지메일 스니펫, 메시지 기능 구현

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
-const electron = require('electron')
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
-
+const { app, BrowserWindow } = require('electron');
+const { ipcMain } = require('electron');
 const path = require('path')
 const url = require('url')
+const electronOauth2 = require('electron-oauth2');
 
 // 윈도우 객체를 전역에 유지합니다. 만약 이렇게 하지 않으면
 // 자바스크립트 GC가 일어날 때 창이 멋대로 닫혀버립니다.
 let mainWindow
 
-function createWindow () {
+// google auth2 설정
+var config = {
+  clientId: '548782842110-q2ulkkpmug2r1iq9r65pladgqj1jgloc.apps.googleusercontent.com',
+  clientSecret: 'UyvPhortjl2ppSH3cOP0OKQY',
+  authorizationUrl: 'https://accounts.google.com/o/oauth2/auth',
+  tokenUrl: 'https://accounts.google.com/o/oauth2/token',
+  useBasicAuthorizationHeader: false,
+  redirectUri: 'http://localhost:8000'
+};
+
+function createWindow() {
 
   // 새로운 브라우저 창을 생성합니다.
-  mainWindow = new BrowserWindow({width: 1200, height: 550})
+  mainWindow = new BrowserWindow({ width: 1200, height: 550 })
 
   // 그리고 현재 디렉터리의 index.html을 로드합니다.
   mainWindow.loadURL(url.format({
@@ -28,6 +37,7 @@ function createWindow () {
   mainWindow.on('closed', function () {
     mainWindow = null
   })
+
 }
 
 // 이 메서드는 Electron의 초기화가 끝나면 실행되며 브라우저
@@ -48,6 +58,27 @@ app.on('activate', function () {
   }
 })
 
-// 이 파일엔 제작할 애플리케이션에 특화된 메인 프로세스 코드를
-// 포함할 수 있습니다. 또한 파일을 분리하여 require하는 방법으로
-// 코드를 작성할 수도 있습니다.
+// 클라이언트 구글 인증 통신
+ipcMain.on('request-auth', (e, args) => {
+  const windowParams = {
+    alwaysOnTop: true,
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: false
+    }
+  }
+
+  const options = {
+    scope: 'https://www.googleapis.com/auth/gmail.readonly'
+  };
+
+  const myApiOauth = electronOauth2(config, windowParams);
+
+  myApiOauth.getAccessToken(options)
+    .then(token => {
+      mainWindow.webContents.send('response-auth', { access_token: token.access_token })
+      myApiOauth.refreshToken(token.refresh_token)
+        .then(newToken => {
+        });
+    });
+});
--- renderer.js
+++ renderer.js
@@ -1,3 +1,119 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+const {ipcRenderer} = require('electron');
+const {BrowserWindow, app} = require('electron').remote;
+const $ = require('jquery');
+
+$('#btn_auth').on('click', function() {
+    ipcRenderer.send('request-auth', '1');
+});
+
+$('#btn_message').on('click', function() {
+    getSnippet()
+});
+
+// 인증완료
+ipcRenderer.on('response-auth', (event, args) => {
+    alert('인증성공!');
+    $('#btn_auth').hide();
+    $('#btn_trello').removeClass('hidden');
+    $('#token').val(args.access_token);
+});
+
+function getProfile() {
+    $.ajax({
+        type: 'get',
+        url: 'https://www.googleapis.com/gmail/v1/users/me/profile',
+        beforeSend : function(xhr){
+            xhr.setRequestHeader("Authorization", "Bearer " + $('#token').val());
+        },
+        success : function(json) {
+           let text = 'email: ' + json.emailAddress
+            + '\nmessagesTotal: ' + json.messagesTotal
+            + '\nthreadsTotal: ' + json.threadsTotal
+           $('#results').html(text)
+        }
+    })
+}
+
+function getMessage() {
+    $.ajax({
+        type: 'get',
+        url: 'https://www.googleapis.com/gmail/v1/users/me/messages',
+        beforeSend :  function(xhr) {
+            xhr.setRequestHeader("Authorization", "Bearer " + $('#token').val())
+        },
+        success : function(json) {
+            $(json.messages).each(function(i, item) {
+                let threadId = item.threadId
+                $.ajax({
+                    type: 'get',
+                    url: 'https://www.googleapis.com/gmail/v1/users/me/messages/' + threadId,
+                    beforeSend : function(xhr) {
+                        xhr.setRequestHeader("Authorization", "Bearer " + $('#token').val())
+                    },
+                    success : function(json) {
+                        let parts_1 = json.payload.parts
+                        if(parts_1 != undefined) {
+                            let mimeType = parts_1[0].mimeType
+                            // mimeType에 따라서 응답구조가 다름.
+                            if(mimeType == 'text/plain') {
+                                // mimeType이 text/plain 일 경우 메일 본문 메시지
+                                let body = parts_1[0].body.data
+                            } else {
+                                // mimeType이 multipart 일 경우 메일 본문 메시지
+                                let parts_2 = parts_1[0].parts[0]
+                                let body = parts_2.body.data
+                            }
+                        }
+                    }
+                })
+            });
+        }
+    })
+}
+
+function getSnippet() {
+    $.ajax({
+        type: 'get',
+        url: 'https://www.googleapis.com/gmail/v1/users/me/messages',
+        beforeSend :  function(xhr) {
+            xhr.setRequestHeader("Authorization", "Bearer " + $('#token').val())
+        },
+        success : function(json) {
+            $(json.messages).each(function(i, item) {
+                let threadId = item.threadId
+                $.ajax({
+                    type: 'get',
+                    url: 'https://www.googleapis.com/gmail/v1/users/me/messages/' + threadId + '?format=metadata',
+                    beforeSend : function(xhr) {
+                        xhr.setRequestHeader("Authorization", "Bearer " + $('#token').val())
+                    },
+                    success : function(json) {
+                        if(json != undefined) {
+                            let subject, to, from, cc, name, elements, headers;
+                            // 보낸사람 필터
+                            let array = ['천영학 <younghak@nkia.co.kr>', '경민성 <wildfire1974@in-soft.co.kr>', '오상영 <syoh@in-soft.co.kr>'];
+                            headers = json.payload.headers
+                            $(headers).each(function(i, item) {
+                                if(item.name == 'From') 
+                                    from = item.value.replace(/\"/g, "");
+                                if(item.name == 'Subject')
+                                    subject = item.value;
+                                if(from != undefined && subject != undefined) {
+                                    for(let k = 0; k < array.length; k++)
+                                        if(from == array[k])
+                                            $('#mailList tbody').append(
+                                                '<tr>'
+                                                    + '<td>' + from + '</td>'
+                                                    + '<td>' + subject + '</td>' +
+                                                '</tr>'
+                                            );
+                                    return false;
+                                }
+                            });
+                        }
+                    }
+                })
+            });
+        }
+    })
+}
